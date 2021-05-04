67ec60e9d8d90d0c524ab543dcdc32789caa855d
juliodparedes <80488323+juliodparedes@users.noreply.github.com>
2021-05-04 18:21:20 -0300

    Add files via upload

---
+++ operadoresRelacionales.py
@@ -0,0 +1,21 @@
+num1, num2, num3 = 10, 20, 30
+resultado=num1<num2
+print(num1 ,"es < que ",num2, ":",resultado)
+print("----------")
+resultado=num1>num2
+print(num1 ,"es > que ",num2, ":",resultado)
+print("----------")
+resultado=num1==num2
+print(num1 ,"es == que ",num2, ":",resultado)
+print("----------")
+resultado=num1<=num2
+print(num1 ,"es <= que ",num2, ":",resultado)
+print("----------")
+resultado=num1>=num2
+print(num1 ,"es >= que ",num2, ":",resultado)
+print("----------")
+resultado=num1!=num2
+print(num1 ,"es != que ",num2, ":",resultado)
+print("----------")
+resultado=num1+num2==num3
+print(num1+num2 ,"es == que ",num3, ":",resultado)
